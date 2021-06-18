40d39ab2957bc4f008bef72a48405fa20d13f877
Precious-B <solomonprecious04@gmail.com>
2021-06-18 09:22:10 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * putchar - to print all numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 48;
+while (i < 58)
+i++;
+putchar(i);
+putchar('\n');
+return (0);
+}
