a2ebf15065d626dd4beec332fdedf6911d6ad2a2
Precious-B <solomonprecious04@gmail.com>
2021-06-22 16:57:34 +0000

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - to print every minute of the day
+ */
+void jack_bauer(void)
+{
+int hr;
+int hr1;
+int min;
+int min1;
+for (hr = 0; hr <= 2; hr++)
+{
+for (hr1 = 0; hr1 <= 3; hr1++)
+{
+for (min = 0; min <= 5; min++)
+{
+for (min1 = 0; min1 <= 9 min1++)
+{
+_putchar(hr + '0');
+_putchar(hr1 + '0');
+_putchar(':');
+_putchar(min + '0');
+_putchar(min1 + '0');
+}
+}
+}
+}
+}
