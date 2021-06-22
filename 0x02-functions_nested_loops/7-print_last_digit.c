bad1d48d1d5ec3a52179cb5d2cd41f42cd8e16a0
Precious-B <solomonprecious04@gmail.com>
2021-06-22 23:31:08 +0000

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,19 +11,12 @@
 int print_last_digit(int n)
 {
 int lastDigit;
-long int lastDigit1;
 if (n < 0)
 {
 n = (-1) * n;
 lastDigit = n % 10;
 _putchar(lastDigit + '0');
 return (lastDigit);
-}
-if (n >= INT_MIN)
-{
-lastDigit1 = n;
-lastDigit1 = (-1) * lastDigit1;
-_putchar(lastDigit1 % 10 + '0');
 }
 lastDigit = n % 10;
 _putchar(lastDigit + '0');
