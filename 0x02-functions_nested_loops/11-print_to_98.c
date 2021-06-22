f0c7fac886e27a8a8b8d6f8f8808286aa9a62002
Precious-B <solomonprecious04@gmail.com>
2021-06-22 19:51:37 +0000

    Natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,18 +9,18 @@
 void print_to_98(int n)
 {
 int i;
- for (i = n; i <= 98; i++)
+for (i = n; i <= 98; i++)
 {
-printf("%d", n);
-if (n != 98)
+printf("%d", i);
+if (i != 98)
 {
 printf(", ");
 }
 }
- for (i = n; i >= 98; i--)
+for (i = n; i > 98; i--)
 {
-printf("%d", n);
-if (n != 98)
+printf("%d", i);
+if (i != 98)
 {
 printf(", ");
 }
