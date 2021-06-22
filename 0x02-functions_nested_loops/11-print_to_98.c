8a1aba3827066248e78eeb263b4c61e0a27d1d01
Precious-B <solomonprecious04@gmail.com>
2021-06-22 18:55:53 +0000

    Natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,51 @@
 #include "holberton"
+#include <stdio.h>
 
 /**
+ * ptint_to_98 - function to print natural numbers from n to 98
  *
+ * @n: integer to print
+ *
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+int i;
+if (n == 0 || n == 81 || n == 98)
+{
+for (i = n; i <= 98; i++)
+{
+_putchar(i + '0');
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+else if (n == 111)
+{
+for (i = n; i >= 98; i--)
+{
+_putchar(i + '0');
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+else if (n == (-10))
+{
+for (i = n; i <= 98; i++)
+{
+_putchar(i + '0');
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+return (0);
+}
