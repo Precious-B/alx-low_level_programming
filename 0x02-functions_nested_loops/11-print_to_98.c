227035da0973485f28090139fbe79569ad0eb3a2
Precious-B <solomonprecious04@gmail.com>
2021-06-22 19:23:20 +0000

    Natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,22 +10,9 @@
  */
 void print_to_98(int n)
 {
-int i;
-if (n <= 98)
-{
-for (i = n; i <= 98; i++)
-{
-_putchar(i + '0');
-if (i != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-}
-else if (n > 98)
-{
-for (i = n; i >= 98; i--)
+int i = n;
+
+ while (i <= 98)
 {
 _putchar(i + '0');
 if (i != 98)
@@ -33,6 +20,17 @@ if (i != 98)
 _putchar(',');
 _putchar(' ');
 }
+i++;
 }
+
+ while (i >= 98)
+{
+_putchar(i + '0');
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+i--;
 }
 }
