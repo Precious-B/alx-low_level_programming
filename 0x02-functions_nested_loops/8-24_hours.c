b3ceedc9f8b44ce5e88ebbccc81a0eab0976ed32
Precious-B <solomonprecious04@gmail.com>
2021-06-22 17:30:11 +0000

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,12 +8,28 @@ void jack_bauer(void)
 int hr, hr1, min, min1;
 for (hr = 0; hr <= 2; hr++)
 {
+if (hr == 2 && hr1 == 4)
+{
+break;
+}
 for (hr1 = 0; hr1 <= 3; hr1++)
 {
+if (hr == 2 && hr1 == 4)
+{
+break;
+}
 for (min = 0; min <= 5; min++)
 {
+if (hr == 2 && hr1 == 4)
+{
+break;
+}
 for (min1 = 0; min1 <= 9; min1++)
 {
+if (hr == 2 && hr1 == 4)
+{
+break;
+}
 _putchar(hr + '0');
 _putchar(hr1 + '0');
 _putchar(':');
