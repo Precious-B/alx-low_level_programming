a1abe490a40934973dd1a5c8a43a06be24ef23d5
Precious-B <solomonprecious04@gmail.com>
2021-06-22 13:58:25 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,26 @@
 #include "holberton.h"
 
 /**
- * 
+ * print_sign - to print the sign of a number
+ *
+ * @n: the integer to be printed
+ *
+ * Return: to return + if number is positive,
+ * returns - if number is negative,
+ * and 0 for 0.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (+);
+}
+else if (n < 0)
+{
+return (-);
+}
+else
+{
+return (0);
+}
+}
