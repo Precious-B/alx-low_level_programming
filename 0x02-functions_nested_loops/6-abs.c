63742abe047f528926d8047eeee2626cda519ffb
Precious-B <solomonprecious04@gmail.com>
2021-06-22 14:24:30 +0000

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _abs -to compute the absolute value of a number
+ *
+ * @int: the number to be computed
+ *
+ * Return: Always 0
+ */
+int _abs(int)
+{
+abs(int);
+return (0);
+}
