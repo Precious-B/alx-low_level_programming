68b5ac120fd40ef119a43665589f98c3c2eeb180
Precious-B <solomonprecious04@gmail.com>
2021-06-22 19:29:11 +0000

    Natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,29 +5,27 @@
  * print_to_98 - function to print natural numbers from n to 98
  *
  * @n: integer to print
- *
- * Return: Always 0
  */
 void print_to_98(int n)
 {
 while (n <= 98)
 {
-_putchar(i + '0');
-if (i != 98)
+_putchar(n + '0');
+if (n != 98)
 {
 _putchar(',');
 _putchar(' ');
 }
-i++;
+n++;
 }
-while (i >= 98)
+while (n >= 98)
 {
-_putchar(i + '0');
-if (i != 98)
+_putchar(n + '0');
+if (n != 98)
 {
 _putchar(',');
 _putchar(' ');
 }
-i--;
+n--;
 }
 }
