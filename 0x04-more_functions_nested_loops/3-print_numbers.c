a74c153cd849cc6751ab6193733382ebc7bffa05
Precious-B <solomonprecious04@gmail.com>
2021-06-24 13:52:14 +0000

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - function to print numbers from 0 to 9
+ *
+ * Return: to return void
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+return (void);
+}
