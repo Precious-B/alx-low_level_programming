a78dbcf41f6ba5dd9768f3c617470287a33a3bcd
Precious-B <solomonprecious04@gmail.com>
2021-06-25 09:48:38 +0000

    Correction

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -23,7 +23,7 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-  if ((month < 1 && day > 31) || (month > 12day >= 31))
+if ((month < 1 && day > 31) || (month > 12))
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
@@ -33,5 +33,4 @@ printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
-return (void);
 }
