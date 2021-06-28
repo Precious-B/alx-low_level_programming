9c62126b985063081ad37c860eadb6d496c29254
Precious-B <solomonprecious04@gmail.com>
2021-06-28 13:20:44 +0000

    I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_rev - a function that prints a string, in reverse, followed by a new line
+ * print_rev - a function that prints a string, in reverse,
+ * followed by a new line
  *
  * @s: pointer to the string to be printed
  */
