9827c782cff8504a8bce860352ceefc0b14f9821
Precious-B <solomonprecious04@gmail.com>
2021-06-30 16:21:35 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,14 +5,16 @@
  *
  * @dest: a pointer to the string to be concatenated
  *
- * @scr: the source string
+ * @src: the source string
+ *
+ * Return: to return a pointer to the destination string
  */
 char *_strcat(char *dest, char *src)
 {
-int index = 0, dest_len  = 0;
+int index = 0, dest_len = 0;
 while (dest[index++])
 dest_len++;
 for (index = 0; src[index]; index++)
-dest[dest_len++] = src [index];
+dest[dest_len++] = src[index];
 return (dest);
 }
