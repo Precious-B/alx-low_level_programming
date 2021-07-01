c80f7f0860d6efc8409285390ce5a23a86556166
Precious-B <solomonprecious04@gmail.com>
2021-07-01 18:06:39 +0000

    Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,8 +3,27 @@
 /**
  * leet - a function that encodes a string into 1337
  *
- * @
+ * @ch: a pointer to the string to be encoded
+ *
+ * Return: to return the result
  */
-char *leet(char *)
+char *leet(char *ch)
 {
+char *s = ch;
+int k, length;
+char *letters = "aeotlAEOTL";
+char *numbers = "4307143071";
+length = 0;
+while (ch[length] != '\0')
+{
+k = 0;
+while (letters[k] != '\0')
+{
+if (ch[length] == letters[k])
+ch[length] = numbers[k];
+k++;
+}
+length++;
+}
+return (s);
 }
