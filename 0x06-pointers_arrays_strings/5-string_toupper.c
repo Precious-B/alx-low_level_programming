c82aa045f2af6facbee93974ffbb6bcca4dce905
Precious-B <solomonprecious04@gmail.com>
2021-07-01 04:36:04 -0700

    To uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,26 @@
 #include "holberton.h"
 
 /**
- * string_toupper - a function that changes all lowercase letters of a string to uppercase
+ * string_toupper - a function that changes all lowercase letters
+ * of a string to uppercase
  *
- * @
+ * @ch: a pointer to the string to be changed to uppercase
+ *
+ * Return: to return the string in uppercase
  */
-char *string_toupper(char *)
+char *string_toupper(char *ch)
 {
+char *s = ch;
+int i, j, length;
+for
+(i = 0;
+ch[i] != '\0';
+i++);
+length = i;
+for (j = 1; j < length; j++)
+{
+if (ch[j] >= 'a' && ch[j] <= 'z')
+ch[j] = ch[j] - 32;
+}
+return (s);
 }
