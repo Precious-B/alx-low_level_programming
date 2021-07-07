6c94575d5c6c1742f54193f0b2641f02636ec873
Precious-B <solomonprecious04@gmail.com>
2021-07-07 20:09:36 +0000

    wild

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,21 @@
 #include "holberton.h"
 
 /**
+ * s_len - a function to check the length of a string
+ *
+ * @s: the string to be checked
+ *
+ * Return: to return the length of the string
+ */
+int s_len(char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return (1 + s_len(s + 1));
+}
+
+/**
  * wildcmp - a function that compares two strings and returns 1 if the strings
  * can be considered identical, otherwise return 0
  *
@@ -13,5 +28,16 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+if (*s1 == '*')
+{
+if (*(s2 + 1) == '*')
+return (wildcmp(s1, s2 + 1));
+if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+return (1);
+}
+return (0);
 }
