e9a95ddad5e705ee5d7914c4db496556e89f4762
Precious-B <solomonprecious04@gmail.com>
2021-07-07 17:39:15 +0000

    Square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,25 @@
 #include "holberton.h"
 
 /**
+ * check_n - a function to check for the square root of a number
+ *
+ * @mid: a possible square root
+ *
+ * @n: a number whose square root is to be found
+ *
+ * Return: to return the square root of @n
+ * and to return -1 if @n has no natural square root
+ */
+int check_n(int  mid, int n)
+{
+if (mid * mid == n)
+return (mid);
+if (mid * mid > n)
+return (-1);
+return (check_n(mid + 1, n));
+}
+
+/**
  * _sqrt_recursion - a function that returns the natural square root of a number
  *
  * @n: an integer value to be passed to the function
@@ -10,5 +29,7 @@
  */
 int _sqrt_recursion(int n)
 {
-
+if (n == 0)
+return (0);
+return (check_n(1, n));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,28 @@
 #include "holberton.h"
 
 /**
- * is_prime_number - a function that returns 1 if the input integer is a prime number, otherwise return 0
+ * check_n - function to check if a nummber is a prime number
+ *
+ * @i: integer used as a fact checker
+ *
+ * @n: number being checked if it is a prme number
+ *
+ * Return: to return 1 if number is a prime number
+ * and 0 if number is not a prme number
+ */
+int check_n(int i, int n)
+{
+if (n < 2 || n % i == 0)
+return (0);
+else if (i > n / 2)
+return (1);
+else
+return (check_n(i + 1, n));
+}
+
+/**
+ * is_prime_number - a function that returns 1 if the input integer
+ * is a prime number, otherwise return 0
  *
  * @n: an integer to be checked if it is a prime number
  *
@@ -10,5 +31,7 @@
  */
 int is_prime_number(int n)
 {
-
+if (n == 2)
+return (1);
+return (check_n(2, n));
 }
