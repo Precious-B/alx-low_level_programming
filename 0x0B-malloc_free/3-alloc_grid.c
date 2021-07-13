535dde6b157ecccd5bd9a4d3ec4f9e3f059973bf
Precious-B <solomonprecious04@gmail.com>
2021-07-13 11:52:39 +0000

    Grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,10 @@
 #include "holberton.h"
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
- * alloc_grid - a function that returns a pointer to a 2 dimensional array of integers
+ * alloc_grid - a function that returns a pointer
+ * to a 2 dimensional array of integers
  *
  * @width: a pointer to the width of the array
  *
@@ -12,5 +15,39 @@
  */
 int **alloc_grid(int width, int height)
 {
-
+int **grid;
+int i, j;
+i = 0;
+if (width <= 0 || height <= 0)
+return (NULL);
+grid = malloc(height * sizeof(*grid));
+if (grid == NULL)
+{
+free(grid);
+return (NULL);
+}
+while (i < height)
+{
+grid[i] = malloc(width * sizeof(**grid));
+if (grid[i] == NULL)
+{
+i--;
+while (i >= 0)
+{
+free(grid[i]);
+i--;
+}
+free(grid);
+return (NULL);
+}
+j = 0;
+while (j < width)
+{
+grid[i][j] = 0;
+j++;
+}
+i++;
+}
+i = 0;
+return (grid);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,9 @@
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
- * free_grid - a function that frees a 2 dimensional grid previously created by the alloc_grid function
+ * free_grid - a function that frees a 2 dimensional grid
+ * previously created by the alloc_grid function
  *
  * @grid: a pointer to a pointer of a function
  *
@@ -9,5 +11,12 @@
  */
 void free_grid(int **grid, int height)
 {
-
+int i;
+i = 0;
+while (i < height)
+{
+free(grid[i]);
+i++;
+}
+free(grid);
 }
