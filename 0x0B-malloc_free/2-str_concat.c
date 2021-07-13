438ba2d33199553dde671997ce3dc91ac6a80d59
Precious-B <solomonprecious04@gmail.com>
2021-07-13 11:18:35 +0000

    Concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,6 +16,14 @@ char *str_concat(char *s1, char *s2)
 char *a;
 int i, length, length2, total_len;
 int j = 0;
+if (s1 == NULL)
+{
+s1 = '\0';
+}
+if (s2 == NULL)
+{
+s2 = '\0';
+}
 for
 (i = 0;
 s1[i] != '\0';
