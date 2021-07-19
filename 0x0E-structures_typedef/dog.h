d14f2a4efa0be65480c5cc22d1b5f4659ff658b7
Precious-B <solomonprecious04@gmail.com>
2021-07-19 15:55:56 +0000

    New name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,12 @@ char *name;
 float age;
 char *owner;
 };
+
+/**
+ * dog_t - a new name for the struct dog
+ */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
