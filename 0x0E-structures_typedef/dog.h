43b488458a14f124b7d6f8595d855b4475a51d5b
Precious-B <solomonprecious04@gmail.com>
2021-07-19 14:32:47 +0000

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a tructure of a dog
+ *
+ * @name: a variable for the name of the dog
+ *
+ * @age: a variable for the age of the dog
+ *
+ * @owner: a variable for the owner of the dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
