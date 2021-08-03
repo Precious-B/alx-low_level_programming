a38f604603877368f03ddc90e3adba8de35bd24a
Precious-B <solomonprecious04@gmail.com>
2021-08-03 16:29:07 +0000

    Reverse

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,5 +9,19 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-
+struct listint_t *prev, *next;
+if (head == NULL || *head == NULL)
+return (NULL);
+if ((*head)->next == NULL)
+return (*head);
+prev = NULL;
+while (*head != NULL)
+{
+next = (*head)->next;
+(*head)->next = prev;
+prev = *head;
+*head = next;
+}
+*head = prev;
+return (*head);
 }
