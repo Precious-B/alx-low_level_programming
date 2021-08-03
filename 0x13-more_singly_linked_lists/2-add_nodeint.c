5bf0ece07eec9e50e0e34dc57eae17d5fb2f3222
Precious-B <solomonprecious04@gmail.com>
2021-08-03 18:51:55 +0000

    Fixing errors

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * add_nodeint - a function that adds a new node at the beginning of a listint_t list
+ * add_nodeint - a function that adds a new node
+ * at the beginning of a listint_t list
  *
  * @head: head of the singly linked list
  *
@@ -13,7 +14,7 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 struct listint_t *new_node;
-new_node = (listint_t*) malloc(sizeof(listint_t));
+new_node = (listint_t *) malloc(sizeof(listint_t));
 if (new_node == NULL)
 {
 free(new_node);
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - a function that returns the nth node of a listint_t linked list
+ * get_nodeint_at_index - a function that returns the nth
+ * node of a listint_t linked list
  *
  * @head: the head f a singly linked list
  *
