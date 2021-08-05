5d387746341e3bfdee4ed98d26139b71b6d7fa06
Precious-B <solomonprecious04@gmail.com>
2021-08-05 16:11:44 +0000

    Print binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,7 +6,8 @@
  * @b: a pointer to a string of 0 and 1 chars
  *
  * Return: the converted number
- * Or returns 0 if there is one or more chars in the string b that is not 0 or 1
+ * Or returns 0 if there is one or more chars
+ * in the string b that is not 0 or 1
  */
 unsigned int binary_to_uint(const char *b)
 {
