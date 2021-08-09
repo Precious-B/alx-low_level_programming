a1f123b95f80aae65ce69c68e5d0ebd595d92a62
Precious-B <solomonprecious04@gmail.com>
2021-08-09 20:49:26 +0000

    Final touches

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -8,22 +8,6 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-/**
- * struct size_t - file structure
- *
- * @filename: the file name
- *
- * @letters: the number of letters
- *
- * Description: file structure
- */
-/**
-typedef struct size_t
-{
-const char *filename;
-struct size_t letters;
-} ssize_t; */
-
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,25 +8,9 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-/**
- * struct size_t - file structure
- *
- * @filename: the file name
- *
- * @letters: the number of letters
- *
- * Description: file structure
- */
-/**
-typedef struct size_t
-{
-const char *filename;
-struct size_t letters;
-} ssize_t; */
-
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
