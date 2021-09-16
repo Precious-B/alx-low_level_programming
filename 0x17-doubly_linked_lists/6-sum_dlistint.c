e7aa8698b1d338e7e7f56e262478d2303cb75bdd
Precious-B <solomonprecious04@gmail.com>
2021-09-16 13:08:22 +0000

    Sum of all the data

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - a function that returns the sum of all the data (n) of a dlistint_t linked list
+ * sum_dlistint - a function that returns the sum of all
+ * the data (n) of a dlistint_t linked list
  *
  * @head: head of the linked list
  *
@@ -10,4 +11,15 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
+dlistint_t *current;
+dlistint_t *next;
+int sum = 0;
+current = head;
+while (current != NULL)
+{
+next = current->next;
+sum += current->n;
+current = next;
+}
+return (sum);
 }
