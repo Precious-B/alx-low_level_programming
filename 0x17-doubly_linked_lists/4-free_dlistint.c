e5631c1cbf4a7ab8fd3e2dbd412097e9bf9928ca
Precious-B <solomonprecious04@gmail.com>
2021-09-16 13:00:14 +0000

    Free a linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,4 +7,13 @@
  */
 void free_dlistint(dlistint_t *head)
 {
+dlistint_t *current;
+dlistint_t *next;
+current = head;
+while (current != NULL)
+{
+next = current->next;
+free(current);
+current = next;
+}
 }
