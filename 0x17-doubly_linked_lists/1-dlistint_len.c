8d29fe37163dba59ade4c1d5b811363c66b91122
Precious-B <solomonprecious04@gmail.com>
2021-09-16 12:18:03 +0000

    number of elements

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,4 +9,12 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+size_t count = 0;
+const dlistint_t *current = h;
+while (current != NULL)
+{
+current = current->next;
+count++;
+}
+return (count);
 }
